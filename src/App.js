effc7c21b34e2386f971f03deb69ec5a86c32710
Antonio Miguel <antoniomiguelborgscorrea@gmail.com>
2023-03-22 22:05:01 -0300

    Adicao de componentes no arquivo AppJS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
+//tela de Login
+import Login from './components/Login/tLogin.js'
+
+//Telas do Cliente
+//import CadastroUsuario from './components/Cliente/tCadastroUsuario.js'
+//import MenuCliente from './components/Cliente/tMenuCliente.js'
+//import Agendamento from './components/Cliente/tAgendamento.js'
+//import CalendarioCliente from './components/Cliente/tCalendarioCliente.js'
+
+//Telas do Profissional
+//import CadastroServico from './components/Profissional/tCadastroServico.js'
+//import MenuProfissional from './components/Profissional/tMenuProfissional.js'
+//import CalendarioProfissional from './components/Profissional/tCalendarioProfissional.js'
+
+//Telas do Administrador
+//import MenuADM from './components/Administrador/tMenuADM.js'
+//import PesquisaFuncionario from './components/Administrador/tPesqFunc.js'
+//import CadastroFuncionario from './components/Administrador/tCadFunc.js'
+//import Agendamentos from './components/Administrador/tAgendamentos.js'
+
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+
+        <Login />
+
+
+        {/* <CadastroUsuario /> */}       
+        {/* <MenuCliente /> */}  
+        {/* <Agendamento /> */}   
+        {/* <CalendarioCliente /> */}    
+
+
+        {/* <CadastroServico /> */}
+        {/* <MenuProfissional /> */}                                              
+        {/* <CalendarioProfissional /> */}  
+
+
+        {/* <MenuADM /> */}        
+        {/* <PesquisaFuncionario /> */}        
+        {/* <CadastroFuncionario /> */}        
+        {/* <Agendamentos /> */}
       </header>
     </div>
   );
