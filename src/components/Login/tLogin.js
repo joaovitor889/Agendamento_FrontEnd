4137c7a4c61e2e8dbb76bf90b812d33b5a63df64
Antonio Miguel <antoniomiguelborgscorrea@gmail.com>
2023-03-24 17:54:47 -0300

    alteracao na responsividade do login

--- src/components/Login/tLogin.js
+++ src/components/Login/tLogin.js
@@ -7,22 +7,28 @@ const telaLogin = () => {
 
    return (        
         <div className = "fLogin">
-            <div className="logo"><h1 title ="Bem-Vindo!">Shostners & shostners</h1></div>            
-            <div className = "fundo">            
-                <form id = "formLogin">    
-                    <h1 title = "Bem-Vindo!">Login</h1>
-                    <div className = "entrada">
-                        <input type="email" placeholder="E-mail" title = "Digite seu E-mail" required/><br></br><br></br>
-                        <input type="password" placeholder="Senha" title = "Digite sua Senha" required/>
+            <div className="container">
+                <div className="row">
+                    <div className="logo"><h1 title ="Bem-Vindo!">Shostners & shostners</h1></div>           
+                </div>
+                <div className="row">
+                    <div className = "fundo">            
+                        <form id = "formLogin">    
+                            <h1 title = "Bem-Vindo!">Login</h1>
+                            <div className = "entrada">
+                                <input type="email" placeholder="E-mail" title = "Digite seu E-mail" required/><br></br><br></br>
+                                <input type="password" placeholder="Senha" title = "Digite sua Senha" required/>
+                            </div>
+                            <div className = "links">
+                                <a href = "./tLogin.js">Criar uma conta</a><br></br>
+                                <a href = "./tLogin.js">Esqueceu a senha?</a>
+                            </div><br></br>
+                            <div className = "botoes">
+                                <input type = "submit" id = "btnLogin" name = "btnLogin" onClick={() => alert('Entra na sua conta se os dados estiverem corretos!')} value = "Login" />                        
+                            </div>  
+                        </form>
                     </div>
-                    <div className = "links">
-                        <a href = "./tLogin.js">Criar uma conta</a><br></br>
-                        <a href = "./tLogin.js">Esqueceu a senha?</a>
-                    </div><br></br>
-                    <div className = "botoes">
-                        <input type = "submit" id = "btnLogin" name = "btnLogin" onClick={() => alert('Entra na sua conta se os dados estiverem corretos!')} value = "Login" />                        
-                    </div>  
-                </form>
+                </div>
             </div>
         </div>
    )
