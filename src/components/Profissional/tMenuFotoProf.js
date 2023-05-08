d953b9e4bf568d03c790ed2f5bd2f6069ddc3371
Antonio Miguel <antoniomiguelborgscorrea@gmail.com>
2023-05-07 23:12:19 -0300

    updateFotoFunc

--- src/components/Profissional/tMenuFotoProf.js
+++ src/components/Profissional/tMenuFotoProf.js
@@ -9,14 +9,53 @@ import Perfil from '../../icones/perfilCliente.png';
 
 import FotoPerfil from '../../icones/UparAlterarPerfilCli.png';
 
+import React, { useState, useEffect } from "react";
+
 
 const TelaFotoProfissional = () => {
 
     document.title = "Foto do Profissional";
 
+    //logica do upload da foto
+    const [selectedFile, setSelectedFile] = useState();
+    const [preview, setPreview] = useState();
+
+
+    useEffect(() => {
+        if (!selectedFile) {
+            setPreview(undefined);
+            return;
+        }
+
+        const objUrl = URL.createObjectURL(selectedFile);
+        setPreview(objUrl);
+
+        //libera memoria quando o componente nao e criado
+        return () => URL.revokeObjectURL(objUrl);
+    }, [selectedFile])
+
+    const onSelectFile = (e) => {
+        if (!e.target.files || e.target.files.lenght === 0) {
+            setSelectedFile(undefined);
+            return;
+        }
+
+        var myPicture = document.getElementById('fotoDefFunc');
+        myPicture.className = styles.desImgDef;
+
+        setSelectedFile(e.target.files[0]);
+    }
+
+    //atualiza foto de perfil
+    const updateFotoFunc = (e) => {
+        e.preventDefault();
+
+        alert('Dados Salvos!');
+    }
+
     return (
-        <div className={styles.fFotoCliente}>
-            <div id={styles["menuLatCli"]}>
+        <div className={styles.fFotoFunc}>
+            <div id={styles["menuLatFunc"]}>
                 <div id={styles["menuDesk"]}>
                     <ul id={styles["ulDesk"]}>
                         <br></br>
@@ -25,7 +64,7 @@ const TelaFotoProfissional = () => {
                             <img src={Perfil} alt="perfil" />
                         </div>
                         <div id={styles["textoLL"]}>
-                            <a href="./tMenuFotoCli" rel="noreferrer">
+                            <a href="./tMenuFotoProf" rel="noreferrer">
                                 <li style={{ backgroundColor: 'rgba(80, 80, 80, 0.5)' }}><p>Foto</p></li>
                             </a>
                         </div>
@@ -33,18 +72,19 @@ const TelaFotoProfissional = () => {
                 </div>
             </div>
 
-            <div id={styles["conteudoCli"]}>
+            <div id={styles["conteudoFunc"]}>
                 <h2><center>Foto (Funcionário)</center></h2>
-                <form id={styles["formFoto"]}>
-                    <center><img src={FotoPerfil} alt="Foto Perfil" /></center>
-                    <center><input type="file" id={styles["fotoCli"]} name="fotoCli" accept="image/jpeg, image/jpg, image/png" required /></center>
-                    <div id={styles["fbtnSalvarotoCli"]}>
-                        <input type="submit" id={styles["btnSalvarFoto"]} name="btnSalvarFoto" onClick={() => alert('Dados Salvos!')} value="Salvar" />
+                <form id={styles["formFotoFunc"]} onSubmit={updateFotoFunc}>
+                    <center><img id="fotoDefFunc" className={styles.fotDef} src={FotoPerfil} alt="Foto Perfil" /></center>
+                    <center>{selectedFile && <img src={preview} alt="Foto Perfil" />}</center>
+                    <center><input type="file" id={styles["fotoFunc"]} name="fotoFunc" accept="image/jpeg, image/jpg, image/png" onChange={onSelectFile} required /></center>
+                    <div id={styles["fbtnSalvarotoFunc"]}>
+                        <input type="submit" id={styles["btnSalvarFoto"]} name="btnSalvarFoto" value="Salvar" />
                     </div>
                 </form>
             </div>
 
-            <div id={styles["menuHorCli"]}>
+            <div id={styles["menuHorFunc"]}>
 
 
                 <div className={styles.perfil}><img src={Perfil} alt="perfil" /></div>
