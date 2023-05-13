2faac6df840521343fb3563f6fb986d189e6b2e9
Antonio Miguel <antoniomiguelborgscorrea@gmail.com>
2023-05-13 12:14:10 -0300

    alteracaoAPI

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const agFetch = axios.create({
-    //baseURL: "http://localhost:4000/api",
+    //API local
     baseURL: "http://localhost:4000",
+    //API Online
+    //baseURL: "http://ec2-34-234-67-50.compute-1.amazonaws.com:4000",
     headers: {
         "Content-Type": "application/json"
     },
